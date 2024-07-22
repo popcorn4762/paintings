1056a01e6a21cbe137e95b5f781b03fd5e496e29
popcorn4762 <zoedong2009@gmail.com>
2024-07-22 12:26:14 -0700

    next button functioning, image centers

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+let selected;
+let cycles = 5;
+let current = 99999;
+
 function buttonClick(id){
-  console.log("hi")
+  selected = id
   document.getElementById(id).classList.add("clicked")
-  document.getElementById("screen").setAttribute("id", "shown")
+  document.getElementById(id + "Img").classList.add("clicked2")
+  document.getElementById("screen").classList.add("shown")
+}
+
+function cancel(){
+  document.getElementById(selected).classList.remove("clicked")
+  document.getElementById(selected + "Img").classList.remove("clicked2")
+  document.getElementById("screen").classList.remove("shown")
+  selected = null;
+}
+
+function next(){
+  if(selected != null){
+    if(current >= cycles)
+      current = 1;
+    else
+      current++;
+    document.getElementById(selected + "Img").src = "./assets/" + selected + "/" + current + ".JPG"
+  }
 }
